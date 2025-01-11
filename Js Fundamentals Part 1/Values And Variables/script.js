c6d437efa4d43c5ca4efcfbd6e229fd88b18eb9f
Soad Ahmed <soadahmed1498@gmail.com>
2025-01-11 17:00:17 +0200

    solved

---
+++ Js Fundamentals Part 1/Values And Variables/script.js
@@ -0,0 +1,6 @@
+let country, continent, population;
+country = "Egypt";
+continent = "Africa";
+population = 109.3;
+
+console.log(`Country :${country} \ncontinent : ${continent} \npopulation : ${population}`);
